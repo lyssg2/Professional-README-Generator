5c8ebfc03891f8859290096ebe097d8435a34757
Lyss Garcia <alyssa.g200@gmail.com>
2021-11-09 20:59:56 -0800

    added validation for responses

--- index.js
+++ index.js
@@ -9,48 +9,111 @@ const questions = [
     {
         input: 'input',
         message: 'What is your project title?',
-        name: 'title'
+        name: 'title',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         input: 'input',
         message: 'What is the description of your project?',
-        name: 'description'
+        name: 'description',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         input: 'input',
         message: 'What are the installation instructions?',
         name: 'installation',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         input: 'input',
         message: 'what is the usage information?',
-        name: 'usage'
+        name: 'usage',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         input: 'input',
         message: 'What are the contribution guidelines?',
-        name: 'contribution'
+        name: 'contribution',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         input: 'input',
         message: 'What are the test instructions?',
-        name: 'test'
+        name: 'test',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         type: "list",
         message: 'What kind of license would you like for your application?',
         choices: ["APACHE 2.0", "GNU LGPLv3", "BSD 3", "IPL 1.0", "ISC", "MIT", "MPL 2.0", "None"],
-        name: 'license'
+        name: 'license',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         input: 'input',
         message: 'What is your GitHub username?',
-        name: 'github'
+        name: 'github',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
     {
         input: 'input',
         message: 'What is your email address?',
-        name: 'email'
+        name: 'email',
+        validate: (response) => {
+            if (!response) {
+                console.log('Please include a response')
+            } else {
+                return true
+            }
+        }
     },
 ];
 
